911b57927a75b05889f01f5a21467fef0ce7bd3f
Riley Wood <rjw245@gmail.com>
2016-04-09 15:28:04 -0400

    Matrix multiply just needs to be parallelized in Rust

--- c/matrixMultiply.rs
+++ c/matrixMultiply.rs
@@ -1,5 +1,5 @@
 const TOTAL_SIZE:       usize = 10;
-const NTHREADS:         usize = 1;
+const NTHREADS:         usize = 4;
 
 fn main(){
     let mut a : Vec<Vec<f32>> = Vec::new(); 
@@ -8,21 +8,27 @@ fn main(){
 
     init_matrix(&mut a, TOTAL_SIZE);
     init_matrix(&mut b, TOTAL_SIZE);
+    init_matrix(&mut c, TOTAL_SIZE);
+    for t in 0..NTHREADS {
+        multiply(&a, &b, &mut c, t);
+    }
 
-    multiply(&a, &b, &mut c, 0);
-
+    print_matrix(&a);
+    println!("*");
+    print_matrix(&b);
+    println!("=");
     print_matrix(&c);
 }
 
-fn multiply(a: Vec<Vec<f32>>, b: Vec<Vec<f32>>, &mut c: Vec<Vec<f32>>, slice: usize){
+fn multiply(a: &Vec<Vec<f32>>, b: &Vec<Vec<f32>>, c: &mut Vec<Vec<f32>>, slice: usize){
     let s = slice;
     let from = (s * TOTAL_SIZE) / NTHREADS;
     let to   = ((s+1) * TOTAL_SIZE) / NTHREADS;
     for i in from..to {
         for j in 0..TOTAL_SIZE {
-            c[i][j] = 0;
+            c[i][j] = 0.0;
             for k in 0..TOTAL_SIZE {
-                c[i][j]+=a[i][k] * b[i][k];
+                c[i][j]+=a[i][k] * b[k][j];
             }
         }
     }
