b06d4eb9f9b61c43eaedad4ad9849cd286fcd0e9
Riley Wood <rjw245@gmail.com>
2016-04-16 16:42:54 -0400

    In the process of doing file input in Rust

--- benchmarks/blackscholes/rust/bs.rs
+++ benchmarks/blackscholes/rust/bs.rs
@@ -1,32 +1,59 @@
-#![feature(link_args)]
-#![feature(libc)]
+// #![feature(link_args)]
+// #![feature(libc)]
 
-extern crate libc;
-use libc::c_int;
-use libc::c_float;
 
-const NLOOPS: usize = 100000000;
+// extern crate libc;
+// use libc::c_int;
+// use libc::c_float;
+use std::fs::File;
+use std::io::{self, BufReader};
 
-#[link_args = "-L./ -lbs"]
-#[link(name = "bs", kind="static")]
+// #[link_args = "-L./ -lbs"]
+// #[link(name = "bs", kind="static")]
 
-extern {
-    fn BlkSchlsEqEuroNoDiv( sptprice: c_float,
-                            strike: c_float, rate: c_float, volatility: c_float,
-                            time: c_float, otype: c_int, timet: c_float ) -> c_float;
+// extern {
+//     fn BlkSchlsEqEuroNoDiv( sptprice: c_float,
+//                             strike: c_float, rate: c_float, volatility: c_float,
+//                             time: c_float, otype: c_int, timet: c_float ) -> c_float;
+// }
+
+struct TestParams {
+    otype: c_int,
+    sptprice: c_float,
+    strike: c_float,
+    rate: c_float,
+    volatility: c_float,
+    otime: c_float,
 }
 
 fn main(){
-    let sptprice: c_float = 1.0;
-    let strike: c_float = 1.0;
-    let rate: c_float = 1.0;
-    let volatility: c_float = 1.0;
-    let time: c_float = 1.0;
-    let otype: c_int = 1;
-    let timet: c_float = 1.0;
-    for i in 0..NLOOPS {
-        let s = unsafe{ BlkSchlsEqEuroNoDiv(sptprice, strike, rate, volatility, time, otype, timet ) };
-        // println!("{}",s);
+    let testpath = "input.txt";
+    let mut tests: Vec<TestParams> = Vec::new();
+    loadTestData(&mut tests, testpath);
+    // let sptprice: c_float = 1.0;
+    // let strike: c_float = 1.0;
+    // let rate: c_float = 1.0;
+    // let volatility: c_float = 1.0;
+    // let time: c_float = 1.0;
+    // let otype: c_int = 1;
+    // let timet: c_float = 1.0;
+    // for i in 0..NLOOPS {
+    //     let s = unsafe{ BlkSchlsEqEuroNoDiv(sptprice, strike, rate, volatility, time, otype, timet ) };
+    //     // println!("{}",s);
+    // }
+}
+
+fn loadTestData(tests: &mut Vec<TestParams>, testpath: String) {
+    let testfile = try!(File::open(testpath));
+    let testfile = BufReader::new(f);
+    for test in testfile.lines() {
+        let numbers: Vec<f32> = test.splite_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect();
+        for num in numbers {
+            println!("{}",num);
+        }
     }
 }
+
 
