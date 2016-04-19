27548b3c7877839c390502fbbb2b95bc1a9cdacc
Riley Wood <rjw245@gmail.com>
2016-04-18 23:24:44 -0400

    Made it so running 1 thread of dot product in Rust avoids spawning a thread. Fully serialized

--- benchmarks/dot_product/rust/dotproduct.rs
+++ benchmarks/dot_product/rust/dotproduct.rs
@@ -12,29 +12,34 @@ fn main() {
     let mut y: Vec<f32> = vec![0.0; N];
     init_vecs(&mut x, &mut y);
 
-    let x_arc = Arc::new(x);
-    let y_arc = Arc::new(y);
-    let mut handles = Vec::new();
-    for t in 0..THREADS {
-        let x_shared = x_arc.clone();
-        let y_shared = y_arc.clone();
-        let handle = thread::spawn(move || {
-            println!("Spawned thread");
-            let start   = t    *N / THREADS;
-            let mut end = (t+1)*N / THREADS;
-            if(t==THREADS-1) { end = N; }
-            let partial_dotprod: f32 = dot_prod(&x_shared,&y_shared,start,end);
-            partial_dotprod
-        });
-        handles.push(handle);
-    }
+    if THREADS==1 {
+        let dotprod = dot_prod(&x, &y, 0, N);
+        println!("{}",dotprod);
+    } else { 
+        let x_arc = Arc::new(x);
+        let y_arc = Arc::new(y);
+        let mut handles = Vec::new();
+        for t in 0..THREADS {
+            let x_shared = x_arc.clone();
+            let y_shared = y_arc.clone();
+            let handle = thread::spawn(move || {
+                println!("Spawned thread");
+                let start   = t    *N / THREADS;
+                let mut end = (t+1)*N / THREADS;
+                if(t==THREADS-1) { end = N; }
+                let partial_dotprod: f32 = dot_prod(&x_shared,&y_shared,start,end);
+                partial_dotprod
+            });
+            handles.push(handle);
+        }
 
-    let mut dotprod: f32 = 0.0;
+        let mut dotprod: f32 = 0.0;
 
-    for handle in handles {
-        dotprod += handle.join().unwrap();
+        for handle in handles {
+            dotprod += handle.join().unwrap();
+        }
+        println!("{}",dotprod);
     }
-    println!("{}",dotprod);
 }
 
 fn init_vecs(x: &mut Vec<f32>, y: &mut Vec<f32>) {
