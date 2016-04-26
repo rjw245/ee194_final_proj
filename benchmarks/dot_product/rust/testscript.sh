e5e35359b623a8fb9ab6067a24ae2c18eaec409a
joshpfosi <joshpfosi@gmail.com>
2016-04-26 19:57:37 -0400

    Switched dot product (rust) to use mutex

--- benchmarks/dot_product/rust/testscript.sh
+++ benchmarks/dot_product/rust/testscript.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in 1 2 4 8 16 ; do (~/ee194/sniper/run-sniper -d thread$i\_output -c gainestown -c nehalem.cfg -n 8 -- ./dotproduct $i &) ; done
+for i in 1 2 4 8 ; do (~/Desktop/Comp140/sniper/run-sniper -d thread$i\_output -c gainestown -c nehalem.cfg -n 8 -- ./dotproduct $i &) ; done
