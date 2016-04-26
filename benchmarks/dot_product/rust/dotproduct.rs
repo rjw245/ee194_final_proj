7a22864a4ee7456956e7cbb40e8946b11a8ac7ce
joshpfosi <joshpfosi@gmail.com>
2016-04-26 16:43:51 -0400

    Rust dot product uses mutexes

--- benchmarks/dot_product/rust/dotproduct.rs
+++ benchmarks/dot_product/rust/dotproduct.rs
@@ -1,7 +1,7 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 use std::env;
-
+use std::thread::JoinHandle;
 const N: usize          = 1048576;
 
 fn main() {
@@ -11,6 +11,8 @@ fn main() {
     let mut x: Vec<f32> = vec![0.0; N];
     let mut y: Vec<f32> = vec![0.0; N];
     init_vecs(&mut x, &mut y);
+    
+    let dotprod = Arc::new(Mutex::new(0.0));
 
     if THREADS==1 {
         let dotprod = dot_prod(&x, &y, 0, N);
@@ -18,27 +20,29 @@ fn main() {
     } else { 
         let x_arc = Arc::new(x);
         let y_arc = Arc::new(y);
-        let mut handles = Vec::new();
+        let mut handles : Vec<JoinHandle<()>> = Vec::new();
+
         for t in 0..THREADS {
             let x_shared = x_arc.clone();
             let y_shared = y_arc.clone();
+            let dotprod_shared = dotprod.clone();
+
             let handle = thread::spawn(move || {
-                println!("Spawned thread");
                 let start   = t    *N / THREADS;
                 let mut end = (t+1)*N / THREADS;
-                if(t==THREADS-1) { end = N; }
-                let partial_dotprod: f32 = dot_prod(&x_shared,&y_shared,start,end);
-                partial_dotprod
+                if t==THREADS-1 { end = N; }
+
+                *dotprod_shared.lock().unwrap() += dot_prod(&x_shared,&y_shared,start,end);
             });
+
             handles.push(handle);
         }
 
-        let mut dotprod: f32 = 0.0;
-
         for handle in handles {
-            dotprod += handle.join().unwrap();
+            handle.join();
         }
-        println!("{}",dotprod);
+
+        println!("{}", *dotprod.lock().unwrap());
     }
 }
 
@@ -53,7 +57,7 @@ fn init_vecs(x: &mut Vec<f32>, y: &mut Vec<f32>) {
 fn dot_prod(x: & Vec<f32>, y: & Vec<f32>, start: usize, end: usize) -> f32 {
     let mut dotprod: f32 = 0.0;
     for i in start..end {
-        if(i>=0 && i<x.len()) {
+        if i < x.len() {
             dotprod = dotprod + (x[i] * y[i]);
         }
     }
