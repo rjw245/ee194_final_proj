6e89a90faec1e29c7d1642a99fe97060d5075d05
joshpfosi <joshpfosi@gmail.com>
2016-04-29 17:50:07 -0400

    Finished dot product local sum sims

--- benchmarks/dot_product/c/test_local_sum/testscript.sh
+++ benchmarks/dot_product/c/test_local_sum/testscript.sh
@@ -1,6 +1,6 @@
 SNIPER_DIR=~/Desktop/Comp140/sniper
 
-for i in 8
+for i in 1 2 4 8
 do
   make NT=$i
   $SNIPER_DIR/run-sniper -d thread$i\_output -n 8 -c gainestown -c nehalem.cfg --roi -- ./dot_product
