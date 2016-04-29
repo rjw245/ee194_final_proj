0763a9568b8314312afa32d91a981021297b2fbc
joshpfosi <joshpfosi@gmail.com>
2016-04-29 10:18:11 -0400

    Ran dot product benchmarks

---
+++ benchmarks/dot_product/c/test_with_locks/testscript.sh
@@ -0,0 +1,7 @@
+SNIPER_DIR=~/Desktop/Comp140/sniper
+
+for i in 1 2 4 8
+do
+  make NT=$i
+  $SNIPER_DIR/run-sniper -d thread$i\_output -n 8 -c gainestown -c nehalem.cfg --roi -- ./dot_product
+done
